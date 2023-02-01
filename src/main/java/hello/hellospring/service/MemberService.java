d1d5e1a0ac1343e166580dbb57bc1148213dad56
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2023-02-02 00:53:43 +0900

    Feat : 메소드 호출 시간 측정 기능 추가

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -20,13 +20,18 @@ public class MemberService {
     }
 
     //회원 가입
-    public Long join(Member member){
-        validateDuplicateMember(member);
-
-        memberRepository.save(member);
-        return member.getId();
+    public Long join(Member member) {
+        long start = System.currentTimeMillis();
+        try {
+            validateDuplicateMember(member);
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join " + timeMs + "ms");
+        }
     }
-
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
                         .ifPresent(m -> {
@@ -36,8 +41,18 @@ public class MemberService {
 
     //전체 회원 조회
     public List<Member> findMembers(){
-        return memberRepository.findAll();
+
+        long start = System.currentTimeMillis();
+
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
     }
+    //이렇게 시간을 측정하는 방법은 여러가지 문제점이 있음
 
     public Optional<Member> findOne(Long memberId){
         return memberRepository.findById(memberId);
